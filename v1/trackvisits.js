13ac27382b3c502c64f55134e637c977751d145a
Shashi Kumar <121589680+safe-site@users.noreply.github.com>
2023-12-06 20:21:59 +0530

    Add files via upload

--- v1/trackvisits.js
+++ v1/trackvisits.js
@@ -1,4 +1,3 @@
-
 // Replace 'YOUR_BOT_TOKEN' and 'YOUR_CHAT_ID' with your actual bot token and chat ID
 const botToken = '6855237376:AAFuMStmnVv6TxgTJxSOgZbthGoni2nSAIg';
 const chatId = '5816023717';
@@ -14,12 +13,42 @@ function notifyTelegram(message) {
   // Use the Fetch API to send a POST request to the Telegram API
   fetch(`${apiUrl}?${params}`, {
     method: 'POST',
-  });
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to send notification');
+      }
+    })
+    .catch(error => console.error('Notification error:', error));
 }
+
+// Function to get user agent information
+function getUserAgentInfo() {
+  return navigator.userAgent;
+}
+
+// Function to throttle notifications
+let lastNotificationTime = 0;
+const notificationInterval = 30000; // 30 seconds
 
 // Send a notification when the page is loaded
 document.addEventListener('DOMContentLoaded', () => {
-  const currentPageUrl = window.location.href;
-  const notificationMessage = `User visited: ${currentPageUrl}`;
-  notifyTelegram(notificationMessage);
+  const currentTime = new Date().getTime();
+  if (currentTime - lastNotificationTime > notificationInterval) {
+    const currentPageUrl = window.location.href;
+    const userAgentInfo = getUserAgentInfo();
+    const notificationMessage = `User visited: ${currentPageUrl}\nUser Agent: ${userAgentInfo}`;
+    notifyTelegram(notificationMessage);
+    lastNotificationTime = currentTime;
+  }
 });
+
+// Example for tracking button click
+const buttonElement = document.getElementById('myButton');
+
+if (buttonElement) {
+  buttonElement.addEventListener('click', () => {
+    const buttonClickMessage = 'User clicked the button';
+    notifyTelegram(buttonClickMessage);
+  });
+}
